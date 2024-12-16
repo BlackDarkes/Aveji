398db712aae5fc1cf1ea92c9d518fb6adad477cf
DaniilGordeev <gordeev.daniil.andreevic@gmail.com>
2024-12-16 23:43:32 +0500

    Aveji

---
+++ script/script.js
@@ -0,0 +1,9 @@
+document.querySelector('#button').onclick = MyClick;
+
+function MyClick() {
+    let a = document.querySelector('forma__name').value;
+    let b = document.querySelector('forma__emale').value;
+    let c = document.querySelector('forma__telephon').value;
+
+    alert(a)
+}
